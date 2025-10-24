48dbfbc8a7490380ea8c5ce0d8014d6d60a81074
MBANS8A1 <seanmbng@aol.com>
2025-10-24 15:31:41 +0100

    I added the select element <option>s with the SelectPercentage component; the percentage values represent the degree of satisfaction of the user and their friend with the service/product.

--- src/App.js
+++ src/App.js
@@ -27,7 +27,18 @@ function BillInput() {
   );
 }
 
-function SelectPercentage() {}
+function SelectPercentage() {
+  return (
+    <div>
+      <select>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
 
 function Output() {}
 
