a000f17073ca4846a6d147c0b9980386fbfc5b07
MBANS8A1 <seanmbng@aol.com>
2025-10-25 10:18:55 +0100

    I added the derived state calculation for the tip in the parent TipCalculator component.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ function TipCalculator() {
   const [bill, setBill] = useState("");
   const [percentage1, setPercentage1] = useState(0);
   const [percentage2, setPercentage2] = useState(0);
+
+  const tip = bill * ((percentage1 + percentage2) / 2 / 100);
+
   return (
     <div>
       <BillInput bill={bill} onSetBill={setBill} />
